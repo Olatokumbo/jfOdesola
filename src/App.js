6d3677174a7cf96b3ad7ad441086f9cac0c248ac
David Odesola <faithodesola@gmail.com>
2021-01-15 12:37:25 +0100

    Working on the Wisdom Quotes Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./Sections/Home/Home";
-import "./App.css";
 import About from "./Sections/About/About";
+import WisdomQuotes from "./Sections/WisdomQuotes/WisdomQuotes";
+import "./App.css";
 const App = () =>{
     return(
         <div>
             <Home/>
             <About/>
+            <WisdomQuotes/>
         </div>
     )
 }
--- src/Sections/WisdomQuotes/WisdomQuotes.js
+++ src/Sections/WisdomQuotes/WisdomQuotes.js
@@ -1,12 +1,22 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
-import style from './WisdomQuotes.module.css';
+import quoteMarks from "../../assets/quotation-marks.png";
+import style from "./WisdomQuotes.module.css";
 
 const WisdomQuotes = () => {
-    return(
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <div className={style.wisdomQuotes}>
+      <Typography className={style.headerTitle}>Wisdom Quotes</Typography>
+      <div className={style.content}>
+        <img src={quoteMarks} alt="quotes" />
+        <Typography className={style.message}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </Typography>
+        <img src={quoteMarks} alt="quotes" />
+      </div>
+    </div>
+  );
+};
 
 export default WisdomQuotes;
